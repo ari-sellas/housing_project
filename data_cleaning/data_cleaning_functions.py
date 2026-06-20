31031c21738e11389ee3c5c5956ba2015f32a701
aristotelis-sellas <295263497+ari-sellas@users.noreply.github.com>
2026-06-20 00:38:07 -0400

    Brought over feature engineering for numerical data

--- data_cleaning/data_cleaning_functions.py
+++ data_cleaning/data_cleaning_functions.py
@@ -33,9 +33,23 @@ def _fit_numeric_cleaners(num_df: pd.DataFrame) -> tuple[pd.DataFrame, list]:
     original_columns = num_df.columns
 
     numeric_imputer = _fit_knn_imputer(num_df)
-    imputed_df = pd.DataFrame(numeric_knn_imputer.transform(num_df), columns=original_columns)
+    imputed_df = pd.DataFrame(numeric_imputer.transform(num_df), columns=original_columns)
 
     ...
+
+
+def _numeric_feature_engineering(num_df: pd.DataFrame) -> pd.DataFrame:
+    num_df = num_df.copy()
+    num_df["TotalLivingSF"] = num_df["TotalBsmtSF"] + num_df["GrLivArea"]
+    num_df["TotalPropertySF"] = num_df["TotalLivingSF"] + num_df["GarageArea"]
+    num_df["AboveGradeBath"] = num_df["FullBath"] + 0.5 * num_df["HalfBath"]
+    num_df["BsmtBath"] = num_df["BsmtFullBath"] + 0.5 * num_df["BsmtHalfBath"]
+    num_df["TotalBath"] = num_df["AboveGradeBath"] + num_df["BsmtBath"]
+    num_df["QualCond"] = num_df["OverallQual"] + num_df["OverallCond"]
+    num_df["TimeBeforeRemod"] = num_df["YearRemodAdd"] - num_df["YearBuilt"]
+    num_df["TimeBeforeSell"] = num_df["YrSold"] - num_df["YearBuilt"]
+    return num_df
+
 
 def _fit_knn_imputer(df: pd.DataFrame, n_neighbors:int = DEFAULT_KNN_NEIGHBORS) -> KNNImputer:
     return KNNImputer(n_neighbors=n_neighbors).fit(df)
