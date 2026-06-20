478f1a1bdf139ab1897ef84f9101d336745ba4d8
aristotelis-sellas <295263497+ari-sellas@users.noreply.github.com>
2026-06-20 12:14:21 -0400

    Changed BASE_MODEL_SPECS to a list instead of a dictionary

--- ensemble_models/ensemble_optimizing_functions.py
+++ ensemble_models/ensemble_optimizing_functions.py
@@ -90,10 +90,11 @@ def _ridge_param_space(trial: optuna.trial.Trial) -> dict:
     }
 
 
-BASE_MODEL_SPECS = {
+BASE_MODEL_SPECS = [
     ModelSpec("xgb_regressor", XGBRegressor, _xgb_param_space, {"n_jobs": -1}),
     ModelSpec("lightgbm_regressor", LGBMRegressor, _lightgbm_param_space, {"n_jobs": -1, "verbose": -1}),
     ModelSpec("catboost_regressor", CatBoostRegressor, _catboost_param_space, {"thread_count": -1, "verbose": -1}),
     ModelSpec("random_forest", RandomForestRegressor, _random_forest_param_space, {"n_jobs": -1}),
     ModelSpec("ridge", Ridge, _ridge_param_space, {})
-}
+]
+
